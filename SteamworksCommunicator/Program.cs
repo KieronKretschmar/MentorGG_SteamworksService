bbf0342db4eea316043f2c23655ead8318e61a61
Kieron <kieron@mentor.gg>
2019-12-23 11:24:33 +0100

    Remove Startup.cs and move contents to Program.cs to enable usage of IHostedService

--- SteamworksCommunicator/Program.cs
+++ SteamworksCommunicator/Program.cs
@@ -1,22 +1,30 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SharingCodeGatherer;
 using System;
 
 namespace SteamworksCommunicator
 {
-    class Program
+    public class Program
     {
-
         public static void Main(string[] args)
         {
-            IServiceCollection services = new ServiceCollection();
-
-            Startup startup = new Startup();
-            startup.ConfigureServices(services);
-            IServiceProvider serviceProvider = services.BuildServiceProvider();
-
+            CreateHostBuilder(args).Start();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddLogging(o =>
+                    {
+                        o.AddConsole();
+                        o.AddDebug();
+                    });
+
+                    services.AddSingleton<ISteamWorksCommunicator, SteamWorksCommunicator>();
+                });
     }
 }
 

35d062552a75604ea0ca9aa93f5eb577c0691c20
Kieron <kieron@mentor.gg>
2019-12-23 12:40:50 +0100

    Add Rabbit Consumer and Producer

---
+++ SteamworksCommunicator/GathererConsumer.cs
@@ -0,0 +1,55 @@
+﻿using RabbitMQ.Client;
+using RabbitTransfer.Consumer;
+using RabbitTransfer.Interfaces;
+using RabbitTransfer.TransferModels;
+using SharingCodeGatherer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SteamworksCommunicator
+{
+    /// <summary>
+    /// Consumer for messages from SharingCodeGatherer.
+    /// Receives all the messages containing SharingCodes, adds information from Steamworks, and publishes them to DemoCentral.
+    /// </summary>
+    public class GathererConsumer : Consumer<SCG_SWS_Model>
+    {
+        private readonly IProducer<GathererTransferModel> _producer;
+        private readonly ISteamWorksCommunicator _swComm;
+
+        public GathererConsumer(IQueueConnection queueConnection, IProducer<GathererTransferModel> producer, ISteamWorksCommunicator swComm) : base(queueConnection)
+        {
+            _producer = producer;
+            _swComm = swComm;
+        }
+
+        public override void HandleMessage(IBasicProperties properties, SCG_SWS_Model inboundModel)
+        {
+            // Get data from Steamworks
+            SteamworksData swData;
+            try
+            {
+                swData = _swComm.GetMatchData(inboundModel.SharingCode);
+            }
+            catch (TimeoutException e)
+            {
+                // TODO: Nack this so it gets requeued
+                throw;
+            }
+
+            // Create outbound model
+            var outboundModel = new GathererTransferModel
+            {
+                DownloadUrl = swData.DownloadUrl,
+                MatchDate = swData.MatchDate,
+                UploaderId = inboundModel.UploaderId,
+                UploadType = inboundModel.UploadType,
+                Source = RabbitTransfer.Enums.Source.Valve,
+            };
+
+            // Publish 
+            _producer.PublishMessage(new Guid().ToString(), outboundModel);
+        }
+    }
+}
--- SteamworksCommunicator/Program.cs
+++ SteamworksCommunicator/Program.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using RabbitTransfer.Interfaces;
+using RabbitTransfer.Producer;
+using RabbitTransfer.Queues;
+using RabbitTransfer.TransferModels;
 using SharingCodeGatherer;
 using System;
 
 namespace SteamworksCommunicator
 {
+    /// <summary>
+    /// 
+    /// Requires environment variables: ["AMQP_URI", "AMQP_GATHERER_QUEUE", "AMQP_DEMOCENTRAL_QUEUE"]
+    /// </summary>
     public class Program
     {
         public static void Main(string[] args)
@@ -24,6 +33,24 @@ namespace SteamworksCommunicator
                     });
 
                     services.AddSingleton<ISteamWorksCommunicator, SteamWorksCommunicator>();
+
+                    // Create producer
+                    var outConnection = new QueueConnection(
+                        hostContext.Configuration.GetValue<string>("AMQP_URI"),
+                        hostContext.Configuration.GetValue<string>("AMQP_DEMOCENTRAL_QUEUE"));
+                    services.AddSingleton<IProducer<GathererTransferModel>>(sp =>
+                    {
+                        return new Producer<GathererTransferModel>(outConnection);
+                    });
+
+                    // Create consumer
+                    var inConnection = new QueueConnection(
+                        hostContext.Configuration.GetValue<string>("AMQP_URI"),
+                        hostContext.Configuration.GetValue<string>("AMQP_GATHERER_QUEUE"));
+                    services.AddHostedService<GathererConsumer>(sp =>
+                    {
+                        return new GathererConsumer(inConnection, sp.GetService<IProducer<GathererTransferModel>>(), sp.GetService<ISteamWorksCommunicator>());
+                    });
                 });
     }
 }
